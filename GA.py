1ba548d96b8c8fb4b1db40cdba60c543588df660
s22a0023-maker <s22a0023@siswa.umk.edu.my>
2026-01-06 13:01:14 +0800

    Update GA.py

--- GA.py
+++ GA.py
@@ -2,9 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import random
-import matplotlib
-matplotlib.use("Agg")
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import time
 
 # -------------------------------
